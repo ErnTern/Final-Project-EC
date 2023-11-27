0597165e7ffa0b51bc1d2ecb1b3f1a8e2b2ba865
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-27 14:14:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@ include "view-header.php";
 ?>
 
 <h1>Welcome</h1>
- <div class="col">
-      <img  display: flex;
+ <div             display: flex;
             justify-content: center;
-            align-items: center;
-       src="https://www.leagueoflegends.com/static/open-graph-b580f0266cc3f0589d0dc10765bc1631.jpg" alt="...">
+            align-items: center; class="col">
+      <img src="https://www.leagueoflegends.com/static/open-graph-b580f0266cc3f0589d0dc10765bc1631.jpg" alt="...">
   </div>
 <?php
 include "view-footer.php";
