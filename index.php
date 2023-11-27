5ad162971f2c8f9c1771f23cece95bde3d6d33fb
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-27 14:11:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
+
 <h1>Welcome</h1>
+ <div class="col">
+    <div class="card">
+      <img src="https://musicart.xboxlive.com/7/f7c15100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Fast & Furious 9</h5>
+        <p class="card-text">6/10 It was still entertaining but the franchise has gone a long way since just being about cars.</p>
+      </div>
+    </div>
+  </div>
 <?php
 include "view-footer.php";
 ?>
