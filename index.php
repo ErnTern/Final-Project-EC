db5aa857aa84f61ee963353171654b603538ff2c
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-28 11:55:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,13 @@ img {
 <body>
 
 <img src="https://www.leagueoflegends.com/static/open-graph-b580f0266cc3f0589d0dc10765bc1631.jpg" alt="...">
+  <h1>Welcome, here is a quick explanation of League of Legends terminology. </h1>
 <p>
   <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        <strong> Champion
+        <strong> Champion </strong>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -30,7 +31,7 @@ img {
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-       <strong> Class
+       <strong> Class </strong>
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -44,7 +45,7 @@ img {
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        <strong> Lane
+        <strong> Lane </strong>
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
