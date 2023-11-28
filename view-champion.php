6ffe19441ae74b903adba3f6602212f3928f023e
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-28 12:17:18 -0600

    Create view-champion.php

---
+++ view-champion.php
@@ -0,0 +1,23 @@
+<h1>Champions</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Name</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($champions = $Champion->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $champions['champ_name']; ?></td>
+    <td><?php echo $champions['class_id']; ?></td>
+    <td><?php echo $champions['lane_id']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
