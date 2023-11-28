569fb97957fcca01847750e85ec6bfd3fe96462e
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-28 12:18:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="champions.php" href="/">Champions</a>
+          <a class="nav-link active" aria-current="champion.php" href="/">Champions</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="lanes.php" href="/">Lanes</a>
