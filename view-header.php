357e3c9097b49d567c5ad4f69d08048cba8c008d
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-28 12:32:38 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -10,7 +10,7 @@
     <div class = "container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="https://pentagram-production.imgix.net/cc7fa9e7-bf44-4438-a132-6df2b9664660/EMO_LOL_02.jpg?rect=0%2C0%2C1440%2C1512&w=640&crop=1&fm=jpg&q=70&auto=format&fit=crop&h=672" alt="..."></a>
+    <a class="navbar-brand" href="#"><img src="file:///C:/Users/aaron/Downloads/kisspng-league-of-legends-computer-icons-riven-riot-games-legends-5aeb2a5fea9ef1.395510301525361247961.png" alt="..."></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
