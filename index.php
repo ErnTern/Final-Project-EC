5ce3b44c3a34bda4e0ca938e1dbe25dc460b8f7d
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-28 11:50:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ img {
   <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Champion
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        <strong> Champion
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
