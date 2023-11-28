36fd4ea07ad7b07092a8c07d761d7101b2f4faa7
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-28 13:01:59 -0600

    Update view-champion.php

--- view-champion.php
+++ view-champion.php
@@ -1,3 +1,9 @@
+<style>
+body {
+    background-image: url('https://st4.depositphotos.com/22949604/24475/v/1600/depositphotos_244751738-stock-illustration-chinese-traditional-oriental-ornament-background.jpg');
+    background-size: cover;
+    background-position: center;
+}
 <h1>Champions</h1>
 <div class="table-responsive">
   <table class="table">
