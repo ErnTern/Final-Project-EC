e4fa095efb2ffefc6eb560693883a2f08067d897
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-29 16:55:12 -0600

    Update filter.php

--- filter.php
+++ filter.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("filter.php");
 
+$pageTitle = "Champion by Class";
+include "view-header.php";
+$champion = selectChampion();
+include "view-filter.php";
+include "view-footer.php";
+?>
