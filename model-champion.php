3c9ea665ca8007500aa5f19b7d9ed530d80960b8
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 17:05:59 -0600

    Update model-champion.php

--- model-champion.php
+++ model-champion.php
@@ -12,4 +12,46 @@ function selectChampion() {
         throw $e;
     }
 }
+
+function addChampion($cName, $cID, $lID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Champion` (`champ_name`, `class_id`, `lane_id`) VALUES (?, ?, ?)");
+        $stmt ->bind_param("sss",$cName, $cID, $lID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateChampion($cName, $cID, $lID, $chID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Champion` SET `champ_name` = ?, `class_id` = ?, `lane_id` = '?' WHERE `Champion`.`champ_id` = ?");
+        $stmt ->bind_param("sssi",$cName, $cID, $lID, $chID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteChampion($chID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE 'Champion' WHERE `champ_id` = ?");
+        $stmt ->bind_param("i",$chID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
