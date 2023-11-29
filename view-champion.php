ad8b652b24086e5a0f96d680e83f4ab02a078bf9
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 17:48:20 -0600

    Update view-champion.php

--- view-champion.php
+++ view-champion.php
@@ -19,6 +19,9 @@ td {
 </style>
     <div class = "container">
 <h1><span class="badge text-bg-light">Champions</span></h1>
+    <?PHP
+    include "view-champion-newform.php"
+    ?>
 <img src="https://cdn.pixabay.com/photo/2017/07/11/13/56/user-2493635_1280.png" width="50" height="auto" alt="...">
 <div class="table-responsive">
   <table class="table">
