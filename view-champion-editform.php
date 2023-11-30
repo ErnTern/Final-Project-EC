943b1a62a90e9501a25c137efc610d49bc1c2abc
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-30 09:49:04 -0600

    Update view-champion-editform.php

--- view-champion-editform.php
+++ view-champion-editform.php
@@ -3,6 +3,14 @@
   <img src="https://p7.hiclipart.com/preview/245/890/350/computer-icons-writing-editing-write.jpg" width="20" height="auto" alt="...">
 </button>
 
+ <form method = "post" action = "">
+            <input type = "hidden" name="chID" value ="<?php echo $champions['champ_id']; ?>">
+            <input type = "hidden" name = "actionType" value = "Delete">
+            <button type="submit" class="btn" onclick = "return confirm('Confirm deletion.');">
+                <img src="https://cdn.imgbin.com/12/12/11/solid-web-buttons-icon-trash-icon-delete-icon-DGwAE9mE.jpg" width="20" height="auto" alt="...">
+            </button>
+            </form>
+
 <!-- Modal -->
 <div class="modal fade" id="editChampModal<?php echo $champions['champ_id']; ?>" tabindex="-1" aria-labelledby="editChampModalLable<?php echo $champions['champ_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
