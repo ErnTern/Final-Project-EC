6d7f5428a0b6f1079e2e065549fd33650f8c9bba
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 19:07:38 -0600

    Update view-champion.php

--- view-champion.php
+++ view-champion.php
@@ -21,14 +21,8 @@ td {
 <h1><span class="badge text-bg-light">Champions</span></h1>
     <?PHP
     include "view-champion-newform.php"
+    include "view-champion-delete.php"
     ?>
- <form method = "post" action = "">
-        <input type = "hidden" name="chID" value ="<?php echo $champion['champ_id']; ?>">
-        <input type = "hidden" name = "actionType" value = "Delete">
-        <button type="submit" class="btn" onclick = "return confirm('Confirm deletion.');">
-            <img src="https://cdn.imgbin.com/12/12/11/solid-web-buttons-icon-trash-icon-delete-icon-DGwAE9mE.jpg" width="50" height="auto" alt="...">
-        </button>
-      </form>
 <div class="table-responsive">
   <table class="table">
     <thead>
