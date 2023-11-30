ace3b121fe4609d137654844cd502640bbd4d0ce
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 18:18:00 -0600

    Update champion.php

--- champion.php
+++ champion.php
@@ -4,6 +4,19 @@ require_once("model-champion.php");
 
 $pageTitle = "Champion";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (addChampion($_POST['cName'], $_POST['cID', $_POST['lID'])) {
+        echo '<div class="alert alert-success" role="alert">New Sport Added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+    
 $champion = selectChampion();
 include "view-champion.php";
 include "view-footer.php";
