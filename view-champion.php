e7a00e4c9d71830234e4b80da88a404d387dfbd7
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 19:21:25 -0600

    Update view-champion.php

--- view-champion.php
+++ view-champion.php
@@ -43,7 +43,7 @@ while ($champions = $champion->fetch_assoc()) {
     </td>
         <td>
             <form method = "post" action = "">
-            <input type = "hidden" name="chID" value ="<?php echo $champion['champ_id']; ?>">
+            <input type = "hidden" name="chID" value ="<?php echo $champions['champ_id']; ?>">
             <input type = "hidden" name = "actionType" value = "Delete">
             <button type="submit" class="btn" onclick = "return confirm('Confirm deletion.');">
                 <img src="https://cdn.imgbin.com/12/12/11/solid-web-buttons-icon-trash-icon-delete-icon-DGwAE9mE.jpg" width="50" height="auto" alt="...">
