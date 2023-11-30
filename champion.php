8bc0783c4ea780733b40618470237f3592350799
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 19:29:19 -0600

    Update champion.php

--- champion.php
+++ champion.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
+    case "Delete":
+      if (deleteChampion($_POST['chID'])) {
+        echo '<div class="alert alert-success" role="alert">Champion Removed.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
   }
 }
     
