e1d2c99a7f2df9c6b981a6f22776fad2a5740af5
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-29 18:22:03 -0600

    Update champion.php

--- champion.php
+++ champion.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (addChampion($_POST['cName'], $_POST['cID', $_POST['lID'])) {
+      if (addChampion($_POST['cName'], $_POST['cID'], $_POST['lID'])) {
         echo '<div class="alert alert-success" role="alert">New Sport Added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
