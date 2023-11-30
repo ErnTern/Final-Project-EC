a3cbbe6a1eb58a961436a922238df0fad30392ae
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-30 09:52:32 -0600

    Update view-champion-editform.php

--- view-champion-editform.php
+++ view-champion-editform.php
@@ -1,9 +1,8 @@
 <!-- Button trigger modal -->
-<p><span>
 <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editChampModal<?php echo $champions['champ_id']; ?>">
   <img src="https://p7.hiclipart.com/preview/245/890/350/computer-icons-writing-editing-write.jpg" width="20" height="auto" alt="...">
 </button>
-</span>
+
  <form method = "post" action = "">
             <input type = "hidden" name="chID" value ="<?php echo $champions['champ_id']; ?>">
             <input type = "hidden" name = "actionType" value = "Delete">
@@ -11,7 +10,7 @@
                 <img src="https://cdn.imgbin.com/12/12/11/solid-web-buttons-icon-trash-icon-delete-icon-DGwAE9mE.jpg" width="20" height="auto" alt="...">
             </button>
             </form>
-</p>
+
 <!-- Modal -->
 <div class="modal fade" id="editChampModal<?php echo $champions['champ_id']; ?>" tabindex="-1" aria-labelledby="editChampModalLable<?php echo $champions['champ_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
