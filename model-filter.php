e130df312a2987297a82e95a8dda953c2a00a90b
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-12-04 16:42:42 -0600

    Update model-filter.php

--- model-filter.php
+++ model-filter.php
@@ -17,7 +17,8 @@ function selectChampion() {
 function selectClass() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT class_id FROM Class");
+        $stmt = $conn->prepare("SELECT champ_id, champ_name, class_id, lane_id FROM `Class`");
+                                //"SELECT class_id FROM Class"
                                 //"SELECT champ_id, champ_name, class_id, lane_id FROM `Champion`" ---- old line 
         $stmt->execute();
         $result = $stmt->get_result();
