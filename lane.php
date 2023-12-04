7258cd47a345b746e29a581c0d733e499d88e446
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-12-04 17:04:23 -0600

    Update lane.php

--- lane.php
+++ lane.php
@@ -1,1 +1,9 @@
+<?php
+require_once("util-db.php");
+require_once("model-filter.php");
 
+$pageTitle = "Lanes Explained";
+include "view-header.php";
+include "view-lane.php";
+include "view-footer.php";
+?>
