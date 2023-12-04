41e9337f5db31f368007c7746073d2629583e3bc
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-12-04 17:29:47 -0600

    Update view-lane.php

--- view-lane.php
+++ view-lane.php
@@ -2,9 +2,22 @@
   <canvas id="myChart"></canvas>
 </div>
 
+
 <?php
-// Example initialization of $countTop
-$countTop = 10;
+function countTop() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT COUNT(lane_id) FROM Champion WHERE lane_id = 'Top'");
+       // $stmt->bind_param("i", $schIDD);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
