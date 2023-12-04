34d0629959fa58a08ee6b1818b31c7892746c77e
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-12-04 14:13:14 -0600

    Update model-filter.php

--- model-filter.php
+++ model-filter.php
@@ -14,6 +14,21 @@ function selectChampion() {
     }
 }
 
+function selectClass() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT class_id FROM Class");
+                                //"SELECT champ_id, champ_name, class_id, lane_id FROM `Champion`" ---- old line 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function selectClassByChampion($iid) {
     try {
         $conn = get_db_connection();
