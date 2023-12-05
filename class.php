c5383af836e1ba1b14672ce95c7a42d9b7f3a4d2
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-12-04 18:14:29 -0600

    Create class.php

---
+++ class.php
@@ -0,0 +1,9 @@
+<?php
+require_once("util-db.php");
+require_once("model-filter.php");
+
+$pageTitle = "Class Chart";
+include "view-header.php";
+include "view-class.php";
+include "view-footer.php";
+?>
