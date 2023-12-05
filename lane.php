33c11a2936c12ca93fa77a48a5160426fb98dd82
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-12-04 18:15:13 -0600

    Update lane.php

--- lane.php
+++ lane.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-filter.php");
 
-$pageTitle = "Lanes Explained";
+$pageTitle = "Lane Chart";
 include "view-header.php";
 include "view-lane.php";
 include "view-footer.php";
