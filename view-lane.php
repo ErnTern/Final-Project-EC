e49ff5140be40a10615c20f067a0769d6eb511b4
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-12-06 12:24:39 -0600

    Update view-lane.php

--- view-lane.php
+++ view-lane.php
@@ -87,7 +87,7 @@ function countSup() {
 $countSup = countSup();
 ?>
 
-<div id="myChartContainer" style="width: 100%; max-width: 400px; height: 600px; margin: auto;">
+<div id="myChartContainer" style="width: 100%; max-width: 400px; height: 2000px; margin: auto;">
   <canvas id="myChart"></canvas>
 </div>
 
