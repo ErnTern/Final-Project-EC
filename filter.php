55345296566a403d429e21ccffc1d0fa8b521bd2
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-12-06 12:15:21 -0600

    Update filter.php

--- filter.php
+++ filter.php
@@ -5,7 +5,7 @@ require_once("model-filter.php");
 $pageTitle = "Champion by Class";
 include "view-header.php";
 //$champion = selectChampion();
-$class1= selectChampion();
+$class1= selectClass();
 include "view-filter.php";
 include "view-footer.php";
 ?>
